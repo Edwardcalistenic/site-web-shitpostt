0f2d6e681b62a0c43858a1d84f7891e3822809ea
Edwardcalistenic <eduardoferreiraaa01@gmail.com>
2025-12-27 15:56:34 -0300

    Atualização:foto/gif/vídeos

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 function mudarNome(){
      let nome = "Esquisito!" 
      document.getElementById("Bem-vindo").textContent = "Bem vindo, " + nome
-    }
+}
+
 /*parte do documento fotos/gif*/
 var swiper = new Swiper(".swiper", {
   cssMode:true,
@@ -15,17 +16,5 @@ var swiper = new Swiper(".swiper", {
     el: ".swiper-pagination",
   },
   keyboard:true,
-  });
-
-  
-
-
-
- 
-
-
-
-
-
-
+});
 
